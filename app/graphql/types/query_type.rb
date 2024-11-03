8982e03274afcb3d91149a0271f7c71db8e98e07
Igno C <ignasch2610@gmail.com>
2024-11-03 21:18:57 +0100

    Add GraphQL type and queries

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -2,30 +2,45 @@
 
 module Types
   class QueryType < Types::BaseObject
-    field :node, Types::NodeType, null: true, description: "Fetches an object given its ID." do
-      argument :id, ID, required: true, description: "ID of the object."
-    end
-
-    def node(id:)
-      context.schema.object_from_id(id, context)
-    end
-
-    field :nodes, [Types::NodeType, null: true], null: true, description: "Fetches a list of objects given a list of IDs." do
-      argument :ids, [ID], required: true, description: "IDs of the objects."
-    end
-
-    def nodes(ids:)
-      ids.map { |id| context.schema.object_from_id(id, context) }
-    end
+#    field :node, Types::NodeType, null: true, description: "Fetches an object given its ID." do
+#      argument :id, ID, required: true, description: "ID of the object."
+#    end
+#
+#    def node(id:)
+#      context.schema.object_from_id(id, context)
+#    end
+#
+#    field :nodes, [Types::NodeType, null: true], null: true, description: "Fetches a list of objects given a list of IDs." do
+#      argument :ids, [ID], required: true, description: "IDs of the objects."
+#    end
+#
+#    def nodes(ids:)
+#      ids.map { |id| context.schema.object_from_id(id, context) }
+#    end
 
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :all_quotes, [Types::QuoteType], null: false do
+      argument :ticker, String, required: true
+    end
+    def all_quotes(ticker:)
+      t = Ticker.find_by(name: ticker.upcase)
+      if t.nil?
+        return []
+      end
+      return t.quote
+    end
+
+    field :latest_quote, Types::QuoteType, null: true do
+      argument :ticker, String, required: true
+    end
+    def latest_quote(ticker:)
+      t = Ticker.find_by(name: ticker.upcase)
+      if t.nil?
+        return nil
+      end
+      t.quote.order(timestamp: :desc).first
     end
   end
 end
