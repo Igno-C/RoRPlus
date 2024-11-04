e52288dccffb240f9c28397b0bbc6385464301d7
Igno C <ignasch2610@gmail.com>
2024-11-04 15:33:14 +0100

    Add mutation to insert new quotes

--- app/models/ticker.rb
+++ app/models/ticker.rb
@@ -1,5 +1,5 @@
 class Ticker < ApplicationRecord
   has_many :quote
 
-  validates: :name, presence: true, length: {maximum: 4, minimum: 1}
+  validates :name, presence: true, length: {maximum: 4, minimum: 1}
 end
