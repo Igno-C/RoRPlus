c5e3b8cd27e7f17801a2f414c19c7ca1235d21c8
Igno C <ignasch2610@gmail.com>
2024-11-05 01:46:50 +0100

    Slight improvements to GraphQL tests

--- spec/graphql/mutations/create_quote_spec.rb
+++ spec/graphql/mutations/create_quote_spec.rb
@@ -12,9 +12,11 @@ module Mutations
 
       it "Creates a quote" do
         expect do
-          post "/graphql", params: {query: query(ticker, timestamp, price)}
-          resp = response.body
-        end.to change {Quote.count}.by(1)
+          expect do
+            post "/graphql", params: {query: query(ticker, timestamp, price)}
+            resp = response.body
+          end.to change {Quote.count}.by(1)
+        end.to change {Ticker.count}.by(1)
       end
 
       it "Returns the inserted quote" do
@@ -29,11 +31,14 @@ module Mutations
 
       it "Modifies the quote instead of inserting" do
         expect do
-          post "/graphql", params: {query: query(ticker, timestamp, price)}
-          post "/graphql", params: {query: query(ticker, timestamp, price+10)}
-          resp = response.body
-        end.to change {Quote.count}.by(1)
+          expect do
+            post "/graphql", params: {query: query(ticker, timestamp, price)}
+            post "/graphql", params: {query: query(ticker, timestamp, price+10)}
+            resp = response.body
+          end.to change {Quote.count}.by(1)
+        end.to change {Ticker.count}.by(1)
       end
+
       it "Returns the updated value" do
         data = JSON.parse(resp)
         data = data["data"]["createQuote"]["quote"]
@@ -41,6 +46,16 @@ module Mutations
         expect(data).to include(
           "price" => price+10
         )
+      end
+
+      it "Creates only one Ticker object" do
+        expect do
+          expect do
+            post "/graphql", params: {query: query(ticker, "2024-10-26T22:00:00Z", price)}
+            post "/graphql", params: {query: query(ticker, "2024-10-26T23:00:00Z", price)}
+            post "/graphql", params: {query: query(ticker, "2024-10-26T24:00:00Z", price)}
+          end.to change {Quote.count}.by(3)
+        end.to change {Ticker.count}.by(1)
       end
     end
 
